a46a06b84e84f3c996eef260641c6634c448d86b
PPC80 <pedro_ppc@hotmail.com>
2022-06-01 18:16:36 -0500

    Ejercicio de clase app movil sin errores :v

---
+++ src/app/file-size.pipe.ts
@@ -0,0 +1,23 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+let SIZE_UNITS=['B','KB','MB'];
+let SIZE_UNITS_LARGE=['Bytes','Kilobytes','Megabytes'];
+
+@Pipe({
+  name: 'fileSize'
+})
+export class FileSizePipe implements PipeTransform {
+
+  transform(sizeBytes: number, sizeForm:boolean): string {
+    let sizeUnits = sizeForm ? SIZE_UNITS_LARGE:SIZE_UNITS;
+    let sizeRoundVal = Math.round(Math.log(sizeBytes)/Math.log(1024));
+
+    sizeRoundVal=Math.min(sizeRoundVal, sizeUnits.length-1);
+    let size= sizeBytes/Math.pow(1024,sizeRoundVal);
+    let sizeFormat= Math.round(size*100)/100
+    let sizeUnit= sizeUnits[sizeRoundVal]
+
+    return `${sizeFormat} ${sizeUnit}`;
+  }
+
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,20 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+
+  firebaseConfig : {
+    apiKey: "AIzaSyCgrhaeCK9bbhovbnBD8ReWWaZv-O7ck-g",
+    authDomain: "proyecto-appmovil-2022a.firebaseapp.com",
+    projectId: "proyecto-appmovil-2022a",
+    storageBucket: "proyecto-appmovil-2022a.appspot.com",
+    messagingSenderId: "728856571351",
+    appId: "1:728856571351:web:3ad21ac2c1f25709b4ef92",
+    measurementId: "G-680B5X8DQN"
+  }
 };
+
+
 
 /*
  * For easier debugging in development mode, you can import the following file
